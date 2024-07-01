2a9661ab12cd2914589d5256237a660ef93e508d
faithcherotich <faithcherotich910@gmail.com>
2024-07-01 13:59:00 +0300

    added console.log

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ function distanceFromHqInBlocks(pickupLocation) {
     const headquarters = 42; 
     return Math.abs(pickupLocation - headquarters);
   }
+  console .log(distanceFromHqInBlocks(50))
   
   function distanceFromHqInFeet(pickupLocation) {
     const blocks = distanceFromHqInBlocks(pickupLocation);
-    return blocks * 264; // Each block is 264 feet
+    return blocks * 264; 
   }
+  console .log(distanceFromHqInFeet(50))
   function distanceTravelledInFeet(start, destination) {
     const blocksTravelled = Math.abs(start - destination);
     return blocksTravelled * 264;
   }
+  console .log(distanceTravelledInFeet(34,38))
   function calculatesFarePrice(start, destination) {
     const distance = distanceTravelledInFeet(start, destination);
   
     if (distance <= 400) {
-      return 0; // First 400 feet are free
+      return 0; 
     } else if (distance > 400 && distance <= 2000) {
-      return (distance - 400) * 0.02; // 2 cents per foot after the first 400 feet
+      return (distance - 400) * 0.02; 
     } else if (distance > 2000 && distance <= 2500) {
-      return 25; // Flat fare for distance between 2000 and 2500 feet
+      return 25; 
     } else {
-      return 'cannot travel that far'; // Distance over 2500 feet is not allowed
+      return 'cannot travel that far'; 
     }
   }
+  console .log(calculatesFarePrice(34,32))
   
       
